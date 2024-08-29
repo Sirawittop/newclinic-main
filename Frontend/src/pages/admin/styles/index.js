1f6f7c122cd27aaa2149290249c13c06e36edb69
trintrintrin <163714047+trinloveyou@users.noreply.github.com>
2024-08-29 17:52:30 +0700

    Update index.js

--- Frontend/src/pages/admin/styles/index.js
+++ Frontend/src/pages/admin/styles/index.js
@@ -48,8 +48,8 @@ export const ArrowButton = styled.button`
   cursor: pointer;
   transition: 0.3s;
   &:hover {
-    background: black;
-    color: white;
+    background: red;
+    color: blue;
   }
 `;
 
